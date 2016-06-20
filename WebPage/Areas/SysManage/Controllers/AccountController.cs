abf55bc7a1255d2d322c5fe5514fe2ce6b238ed0
zjhappy1314 <771426089@qq.com>
2016-06-20 16:45:01 +0800

    增加登陆控制器AccountController运行成功

---
+++ WebPage/Areas/SysManage/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common;
+using Service.IService;
+
+namespace WebPage.Areas.SysManage.Controllers
+{
+    public class AccountController : Controller
+    {
+        #region 声明容器
+        /// <summary>
+        /// 用户管理
+        /// </summary>
+        IUserManage UserManage { get; set; }
+        #endregion
+
+        #region 基本视图
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 帮助方法
+        [ValidateAntiForgeryToken]
+        public ActionResult Login( Domain.SYS_USER item )
+        {
+            var json = new JsonHelper() { Msg = "登陆成功", Status = "n" };
+            try
+            {
+                //调用登陆验证接口 返回用户实体类
+                var users = UserManage.UserLogin( item.ACCOUNT.Trim(), item.PASSWORD.Trim() );
+                if ( users != null )
+                {
+                    //是否锁定
+                    if ( users.ISCANLOGIN == 1 )
+                    {
+                        json.Msg = "用户已锁定，禁止登陆，请联系管理员";
+                        return Json( json );
+                    }
+                    json.Status = "y";
+                }
+                else
+                {
+                    json.Msg = "用户名或者密码不正确";
+                }
+            }
+            catch ( Exception e )
+            {
+                json.Msg = e.Message;
+            }
+            return Json( json, JsonRequestBehavior.AllowGet );
+        }
+        #endregion
+
+    }
+}
--- WebPage/Global.asax.cs
+++ WebPage/Global.asax.cs
@@ -10,7 +10,7 @@ namespace WebPage
 {
     // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : Spring.Web.Mvc.SpringMvcApplication//System.Web.HttpApplication
     {
         protected void Application_Start()
         {
